fb20bb78065dd66e49af5fdcf3c4b3b915c18f77
damragencc <damragenc@gmail.com>
2024-03-20 16:15:51 +0300

    JUnit

---
+++ src/test/java/day03_Maven/C01_MavenilkTest.java
@@ -0,0 +1,29 @@
+package day03_Maven;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import utilities.ReusableMethods;
+
+import java.time.Duration;
+
+public class C01_MavenilkTest {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://www.testotomasyonu.com");
+
+
+        ReusableMethods.bekle(3);
+        driver.quit();
+
+
+
+
+
+
+
+    }
+}
